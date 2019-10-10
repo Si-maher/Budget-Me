c68478213e0e91192890cfb16034f9984b725164
Simon Maher <simonmaher8307@hotmail.com>
2019-10-10 12:45:13 +0300

    created showblance function

--- js/app.js
+++ js/app.js
@@ -38,7 +38,21 @@ submitBudgetForm(){
   }
 }
 showBalance() {
-  console.log(`hey`)
+  const expense = this.totalExpence()
+  const total = parseInt(this.budgetAmount.textContent) - expense
+  this.balanceAmount.textContent =total;
+  if(total < 0) {
+    this.balance.classList.remove('showGreen','showBlack')
+    this.balance.classList.add('showRed')
+  }
+  else if( total > 0){
+    this.balance.classList.remove('showRed', 'showBlack')
+    this.balance.classList.add('showGreen')
+  }
+  else if( total === 0) {
+    this.balance.classList.remove('showRed', 'showGreen')
+    this.balance.classList.add('showBlack')
+  }
 }
 }
 
